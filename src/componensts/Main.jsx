fba2ee879c94b6a05528a5b37d30ff5a199c243f
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-28 15:10:21 +0100

    add main

--- src/componensts/Main.jsx
+++ src/componensts/Main.jsx
@@ -1,9 +1,28 @@
+import Button from "./Button/Button.jsx";
 export default function Main() {
   return (
     <>
       <main>
         <section className="card-main">
-          <div className="container"></div>
+          <div className="container">
+            <div className="card-container col-12">
+              <div className="card">
+                <div className="card-img-container">
+                  <img src="..." className="card-img-top" alt="..." />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">TITOLO DEL POST</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ad, veniam at placeat autem qui, quas ab repellendus vel
+                    modi tempore reprehenderit, temporibus tempora? Nostrum,
+                    esse aut eius atque tenetur sapiente.
+                  </p>
+                  <Button />
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     </>
